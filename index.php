b73111b70ecbf0a4f0c1e80275ff61804ac9113d
LauraAngie <lauraangelica2010@hotmail.com>
2019-10-06 14:09:09 -0500

    cambio de ubicacion de la hoja de estilos y fuentes.css

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="estilos2.css">
-    <link rel="stylesheet" href="fuentes.css">
+    <link rel="stylesheet" href="assets/estilos2.css">
+    <link rel="stylesheet" href="assets/fuentes.css">
     <script src="https://code.createjs.com/1.0.0/createjs.min.js"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
