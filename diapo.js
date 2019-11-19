661506695c33cfcb998856a31c8e6049748152b3
Raul <martinezramirezraul.27@gmail.com>
2019-11-19 16:31:31 -0600

    Acomodo de los botones con style top

--- diapo.js
+++ diapo.js
@@ -1,18 +1,44 @@
 
-var consta = 5;
+var consta = 2;
 var boton;
+var espacio=60;
 function agregar1() {
+
   boton = document.createElement('input');
   boton.type = 'button';
   boton.id = consta;
   boton.className = "bot";
   boton.value = ' ' + consta + ' ';
-  consta = consta + 1;
-  document.getElementById('Panel').appendChild(boton);
+ 
+  if (consta>1){
 
-  var espacio = document.createElement('br');
-  document.getElementById('Panel').appendChild(espacio);
-  var espacio2 = document.createElement('br');
-  document.getElementById('Panel').appendChild(espacio2);
+
+    boton.style.top= (espacio*(boton.id-1))+'px';
+  }
+ consta = consta + 1;
+  document.getElementById('Panel').appendChild(boton);
+  
+
+ 
+
+//flecha arriba 38
+//Fecha abajo 40
+
+  // var espacio = document.createElement('br');
+  // document.getElementById('Panel').appendChild(espacio);
+  // var espacio2 = document.createElement('br');
+  // document.getElementById('Panel').appendChild(espacio2);
 }
 
+
+
+window.onload=function(){
+  document.onkeypress=inspeccionartecla;
+}
+
+function inspeccionartecla(evobjet){
+var elcaracter=evobjet.keyCode;
+  if(elcaracter==40){
+  
+  }
+}
